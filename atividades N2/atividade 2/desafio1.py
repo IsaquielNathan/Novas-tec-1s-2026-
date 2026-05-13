8ba2b1631143c0f5ca3ce49e2e422c12f226e969
IsaquielNathan <isaquielnthan@gmail.com>
2026-05-13 00:11:28 -0300

    Atualizando desafio completo

--- atividades N2/atividade 2/desafio1.py
+++ atividades N2/atividade 2/desafio1.py
@@ -1,5 +1,4 @@
 import pandas as pd
-<<<<<<< HEAD
 
 df = pd.read_csv(
     'vendas.csv', 
@@ -20,7 +19,3 @@ print (df_eletronico.head())
 
 df_media = df.groupby('cidade')['total_venda'].mean().sort_values(ascending=False)
 print(df_media)
-=======
-import numpy as np
-
->>>>>>> f883e5751b2ad0e2c7bd54737a73604b4c9b66c8
